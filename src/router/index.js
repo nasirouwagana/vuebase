a91b4b50d2a739931f39666c16bac6539c473e75
Nasirou Wagana <wnasirou@gmail.com>
2018-04-07 00:17:33 +0100

    Upgraded npm packages

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import VueRouter from 'vue-router'
 import Home from '@/components/Home'
 import Dashboard from '@/components/Dashboard'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
 const routes = [
     {
@@ -18,7 +18,7 @@ const routes = [
     }
 ]
 
-export default new Router({
+export default new VueRouter({
     mode: 'history',
     routes
 })
